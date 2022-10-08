d9edcd90d57c38a0f021c69ddbaaf2e56a0c76d7
hirero765 <forpayment169@gmail.com>
2022-10-08 13:46:47 +0530

    Revert "added BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE for sepolicy neverallows"
    This reverts commit 5de6644ce5e23df92a2e9a805ec19949a566d94e.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,8 +160,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
-
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
